f9f43ad52bf731b69d2ccdc20f524b61747418ad
martaRuiz <martachips@gmail.com>
2024-01-22 11:23:03 +0100

    correction: submit event

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import './style.css';
 import { searchImages } from './src/components/search-images.js/search';
 
-const formSearch = document.querySelector('#search-form');
+// const formSearch = document.querySelector('#search-form');
 const buttonShowMore = document.querySelector('.btn-show-more');
 
-formSearch.addEventListener('submit', (e) => {
-  e.preventDefault(); //previene que el formulario recargue la página
-  page = 1;
-  searchImages();
-});
+// formSearch.addEventListener('submit', (e) => {
+//   e.preventDefault(); //previene que el formulario recargue la página
+//   page = 1;
+//   searchImages();
+// });
 
 document.addEventListener('DOMContentLoaded', () => {
   searchImages(); //se ejecuta cuando todo el HTML del documento ha sido completamente cargado
--- src/components/search-images.js/search.js
+++ src/components/search-images.js/search.js
@@ -1,5 +1,6 @@
 const inputSearch = document.querySelector('#input-text');
 const divAppResults = document.querySelector('.div-app');
+const formSearch = document.querySelector('#search-form');
 let keyword = '';
 let page = 1;
 let perPage = 20;
@@ -14,25 +15,39 @@ export async function searchImages() {
 
   const apiUrlWithApiKey = `https://api.unsplash.com/search/photos?query=${keyword}&per_page=${perPage}&page=${page}&client_id=${apiKey}`;
 
-  const response = await fetch(apiUrlWithApiKey);
-  const data = await response.json();
-  //ahora se controla si es la primera búsqueda, en cuyo caso, se limpia el contenedor:
-  if (page === 1) {
-    divAppResults.innerHTML = '';
+  try {
+    const response = await fetch(apiUrlWithApiKey);
+    if (!response.ok) {
+      throw new Error('Error en la solicitu: ${response.status}');
+    }
+    const data = await response.json();
+
+    //ahora se controla si es la primera búsqueda, en cuyo caso, se limpia el contenedor:
+    if (page === 1) {
+      divAppResults.innerHTML = '';
+    }
+
+    const results = data.results;
+    results.map((result) => {
+      const aImg = document.createElement('a');
+      const img = document.createElement('img');
+      aImg.classList.add('insp-img');
+      img.classList.add('image');
+      img.src = result.urls.small;
+      aImg.href = result.links.html;
+      aImg.target = '_blank';
+
+      aImg.appendChild(img);
+      divAppResults.appendChild(aImg);
+    });
+    page++;
+  } catch (error) {
+    console.error('Error:', error);
   }
-
-  const results = data.results;
-  results.map((result) => {
-    const aImg = document.createElement('a');
-    const img = document.createElement('img');
-    aImg.classList.add('insp-img');
-    img.classList.add('image');
-    img.src = result.urls.small;
-    aImg.href = result.links.html;
-    aImg.target = '_blank';
-
-    aImg.appendChild(img);
-    divAppResults.appendChild(aImg);
-  });
-  page++;
 }
+
+formSearch.addEventListener('submit', (e) => {
+  e.preventDefault(); //previene que el formulario recargue la página
+  page = 1;
+  searchImages();
+});
